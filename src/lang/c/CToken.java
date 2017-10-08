d0d379f57a2a371a9d50aabbead111f4ec8d8a95
QUI44 <cs15035@s.inf.shizuoka.ac.jp>
2017-10-09 03:21:51 +0900

    ver01をひとまず完成

--- src/lang/c/CToken.java
+++ src/lang/c/CToken.java
@@ -4,6 +4,9 @@ import lang.SimpleToken;
 
 public class CToken extends SimpleToken {
 	public static final int TK_PLUS			= 2;				// +
+	public static final int TK_MINUS         = 3;                // -
+	public static final int TK_MUL           = 4;                // *
+	public static final int TK_DIV           = 5;                // /
 
 	public CToken(int type, int lineNo, int colNo, String s) {
 		super(type, lineNo, colNo, s);
